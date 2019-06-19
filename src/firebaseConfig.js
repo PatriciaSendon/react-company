d0f5a27f572018f6a02e6bd4dea59f03d4ab80d4
patricia <patricia.sendon2011@gmail.com>
2019-06-19 11:01:45 -0300

    .

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -1,15 +1,7 @@
 const Rebase = require('re-base')
 const firebase = require('firebase')
 
-const firebaseConfig = {
-  apiKey: "AIzaSyB6rnM3Z5zbPBlEhMShOCnYIGGIgd6BoGk",
-  authDomain: "sendon-portfolio.firebaseapp.com",
-  databaseURL: "https://sendon-portfolio.firebaseio.com",
-  projectId: "sendon-portfolio",
-  storageBucket: "sendon-portfolio.appspot.com",
-  messagingSenderId: "957933978890",
-  appId: "1:957933978890:web:d0083e7c60df4cf3"
-}
+const firebaseConfig = {}
 
 const app = firebase.initializeApp(firebaseConfig)
 const config = Rebase.createClass(app.database())
